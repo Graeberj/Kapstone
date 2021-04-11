3416c0aa763c15a177618fc9cf8c72c445987b11
celus24 <marcelmontgomery2.8@gmail.com>
2021-04-11 11:13:30 -0500

    updated movie title

--- src/component/ImageCard.js
+++ src/component/ImageCard.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { imageRequest } from '../request';
 import Movie from './Movie';
+// import TextTruncate from 'react-text-truncate';
 
 const base_url = 'https://image.tmdb.org/t/p/original/';
 
@@ -8,8 +9,10 @@ function ImageCard({ image }) {
 	return (
 		<div className='imageCard'>
 			<img src={`${base_url}${image.backdrop_path || image.poster_path}`} alt='' />
-			<p>{image.overview}</p>
-			<h2>Movie title</h2>
+
+			{/* <TextTruncate line={1} element='p' truncateText='...' text={image.overview} /> */}
+
+			<h2>{image.title || image.original_name}</h2>
 			<p>number of likes </p>
 		</div>
 	);
