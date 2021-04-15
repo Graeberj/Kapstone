25d849bd77b7b3431d3f94d0e6b1074d0dc0b82b
celus24 <marcelmontgomery2.8@gmail.com>
2021-04-15 12:50:04 -0500

    added icon

--- src/component/ImageCard.js
+++ src/component/ImageCard.js
@@ -1,10 +1,21 @@
 import React from 'react';
+import { useState } from 'react';
+import { HandThumbsDown, HandThumbsUp } from 'react-bootstrap-icons';
 
 // import TextTruncate from 'react-text-truncate';
 
 const base_url = 'https://image.tmdb.org/t/p/original/';
 
-function ImageCard({ image, setMovieOption }) {
+function ImageCard({ image }) {
+	const [fav, setFav] = useState(0);
+
+	function dislikes() {
+		setFav();
+	}
+
+	function likes() {
+		setFav();
+	}
 	return (
 		<div className='imageCard'>
 			<img src={`${base_url}${image.backdrop_path || image.poster_path}`} alt='' />
@@ -12,7 +23,8 @@ function ImageCard({ image, setMovieOption }) {
 			{/* <TextTruncate line={1} element='p' truncateText='...' text={image.overview} /> */}
 
 			<h2>{image.title || image.original_name}</h2>
-			<p>number of likes </p>
+			<HandThumbsUp onClick={() => likes()} className='thumbsUp' />
+			<HandThumbsDown onClick={() => dislikes()} className='thumbsDown' />
 		</div>
 	);
 }
