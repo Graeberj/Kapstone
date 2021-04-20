0a8ffba13b5873b2ec87925cac7b05b5c6521db0
Graeberj <73848091+Graeberj@users.noreply.github.com>
2021-04-20 16:03:13 -0700

    merge

--- src/component/Movie.js
+++ src/component/Movie.js
@@ -29,9 +29,11 @@ function Movie({ movieOption }, props) {
       <div className="fluid-container sb-show-main d-flex flex-column align-items-center imagecard">
         <div className="row row-cols-1 row-cols-md-3 g-4 mx-5">
           {image.map((image) => (
-            <div className="col">
-              <ImageCard key={image.id} image={image} />
-            </div>
+            <Link to={"movie/" + image.id}>
+              <div className="col">
+                <ImageCard key={image.id} image={image} />
+              </div>
+            </Link>
           ))}
         </div>
       </div>
